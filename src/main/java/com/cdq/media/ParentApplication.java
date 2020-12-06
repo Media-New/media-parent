94d3ae467d4255111bf03abde052ce528e9c744e
CaoDongquan <1712446949@qq.com>
2020-12-06 17:49:35 +0800

    初始化media-parent Maven项目

---
+++ src/main/java/com/cdq/media/ParentApplication.java
@@ -0,0 +1,14 @@
+package com.cdq.media;
+
+/**
+ * @author ：ヅてＤＱ
+ * @date ：Created in 2020/12/6 17:21
+ * @description：
+ * @modified By：
+ * @version: 1.0.1
+ */
+public class ParentApplication {
+
+
+
+}
